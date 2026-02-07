e34a6d730f7263e90b0c048c439b1cd4db5cc3f9
Gourav Bhardwaj <gourav_bhardwaj@sfu.ca>
2026-02-06 20:32:32 -0800

    converted tiem from seconds to integer milliseconds for better error-handling

--- main.cpp
+++ main.cpp
@@ -2,18 +2,20 @@
 #include <chrono>
 #include <thread>
 
-int main() {
+int main()
+{
     std::cout << "Hello, world!\n";
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < 300; i++)
+    {
         auto start = std::chrono::steady_clock::now();
 
-        std::this_thread::sleep_for(std::chrono::milliseconds(166));
+        std::this_thread::sleep_for(std::chrono::milliseconds(16));
 
         auto end = std::chrono::steady_clock::now();
 
-        std::chrono::duration<double> elapsed_seconds = end - start;
+        auto elapsed_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
 
-        std::cout << "Elapsed Time " << elapsed_seconds.count() << " seconds\n";
+        std::cout << "Elapsed Time " << elapsed_ms.count() << " ms\n";
     }
 
     return 0;
