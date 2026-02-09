e878b176833ba66fbed36fbce02c84947b601a6e
Gourav Bhardwaj <gourav_bhardwaj@sfu.ca>
2026-02-08 18:38:20 -0800

    Add FrameStats class implementation for frame profiling, including methods for adding samples, calculating averages, and tracking worst frames. Introduced buffer management and reset functionality.

---
+++ profiler/FrameStats.cpp
@@ -0,0 +1,87 @@
+#include "FrameStats.h"
+#include <algorithm>
+
+FrameStats::FrameStats() {
+    frames.resize(bufferSize);
+}
+
+void FrameStats::addSample(int64_t workDuration, int64_t totalDuration, size_t currentFrameIndex) {
+    // Write index is the current frame index modulo the buffer size
+    writeIndex = currentFrameIndex % bufferSize;
+    if (isFull()) {
+        sumWorkUs -= frames[writeIndex].workDuration;
+        sumTotalUs -= frames[writeIndex].totalDuration;
+    }
+    frames[writeIndex] = FrameSample{workDuration, totalDuration, currentFrameIndex};
+    sumWorkUs += workDuration;
+    sumTotalUs += totalDuration;
+
+    // Compare to overall worst frame
+    if (totalDuration > worstFrameOverall.totalDuration){
+        worstFrameOverall = FrameSample{workDuration, totalDuration, currentFrameIndex};
+    }
+
+    // Compare to worst frame in window
+    if (totalDuration > worstFrameWindow.totalDuration){
+        worstFrameWindow = FrameSample{workDuration, totalDuration, currentFrameIndex};
+    }
+
+    // Recalculate worst frame in window it is not a part of the window anymore
+    if (currentFrameIndex - worstFrameWindow.frameIndex > bufferSize - 1){
+        recalculateWorstFrameWindow();
+    }
+
+    count = std::min(count + 1, bufferSize);
+}
+
+// Recalculate the worst frame in window
+void FrameStats::recalculateWorstFrameWindow() {
+    worstFrameWindow = {};
+    for (size_t i = 0; i < bufferSize; i++) {
+        if (frames[i].totalDuration > worstFrameWindow.totalDuration) {
+            worstFrameWindow = frames[i];
+        }
+    }
+}
+
+void FrameStats::reset() {
+    frames.assign(bufferSize, {});
+    writeIndex = 0;
+    count = 0;
+    sumWorkUs = 0;
+    sumTotalUs = 0;
+    worstFrameWindow = {};
+    worstFrameOverall = {};
+}
+
+int64_t FrameStats::getAvgWorkUs() const {
+    return sumWorkUs / count;
+}
+
+int64_t FrameStats::getAvgTotalUs() const {
+    return sumTotalUs / count;
+}
+
+FrameSample FrameStats::getWorstFrameOverall() const {
+    return worstFrameOverall;
+}
+
+FrameSample FrameStats::getWorstFrameWindow() const {
+    return worstFrameWindow;
+}
+
+double FrameStats::getFPS() const {
+    return 1000000.0 / getAvgTotalUs();
+}
+
+size_t FrameStats::getWindowSize() const {
+    return count;
+}
+
+size_t FrameStats::getCapacity() const {
+    return frames.size();
+}
+
+bool FrameStats::isFull() const {
+    return count == frames.size();
+}
--- profiler/FrameStats.h
+++ profiler/FrameStats.h
@@ -7,27 +7,36 @@ struct FrameSample
 {
     int64_t workDuration;
     int64_t totalDuration;
+    size_t frameIndex;
 };
 
 class FrameStats
 {
 private:
     std::vector<FrameSample> frames;
-    int bufferSize = 60;
-    size_t writeIndex;
-    size_t count;
-    int64_t sumWorkUs;
-    int64_t sumTotalUs;
-    int64_t worstTotalUs;
-    int worstFrameIndex;
+    constexpr static size_t bufferSize = 60;
+    size_t writeIndex = 0;
+    size_t count = 0;
+    int64_t sumWorkUs = 0;
+    int64_t sumTotalUs = 0;
+    FrameSample worstFrameWindow = {};
+    FrameSample worstFrameOverall = {};
+    void recalculateWorstFrameWindow();
 
 public:
-    void addSample(int frameIndex, int64_t workDuration, int64_t totalDuration);
+    FrameStats();
+    void addSample(int64_t workDuration, int64_t totalDuration, size_t currentFrameIndex);
+    void reset();
+
+    // Metrics
     int64_t getAvgWorkUs() const;
     int64_t getAvgTotalUs() const;
-    int64_t getWorstTotalUs() const;
-    int getWorstFrameIndex() const;
+    FrameSample getWorstFrameOverall() const;
+    FrameSample getWorstFrameWindow() const;
+    double getFPS() const;
+
+    // Buffer info
     size_t getWindowSize() const;
     size_t getCapacity() const;
-    void reset();
+    bool isFull() const;
 };
